2e1d3478d7afec8e8dfff5ec0ffaeae03e9204e8
Sergey Shcheglov <97shcheglov@gmail.com>
2022-02-08 16:38:45 +0600

    Add URLSession method

--- FriendsApp/ContentView.swift
+++ FriendsApp/ContentView.swift
@@ -8,12 +8,51 @@
 import SwiftUI
 
 struct ContentView: View {
+    @State private var users = [User]()
+    
     var body: some View {
         NavigationView {
-            List {
+            List(users, id: \.id) { user in
+                VStack(alignment: .leading) {
+                    Text(user.name)
+                    Text(user.isActive ? "Online" : "Offline")
+                }
                 
             }
+            .task {
+                await loadData()
+            }
+
+            .navigationTitle("FriendsApp")
         }
+    }
+    
+    func loadData() async {
+        guard let url = URL(string: "https://www.hackingwithswift.com/samples/friendface.json") else {
+            print("Invalid url")
+            return
+        }
+        
+        do {
+            let (data, _) = try await URLSession.shared.data(from: url)
+            
+            let decoder = JSONDecoder()
+            decoder.dateDecodingStrategy = .iso8601
+
+            print("set decodeingStrategy")
+            
+            if let decodedResponse = try? JSONDecoder().decode([User].self, from: data) {
+                print("decoded starts")
+
+                self.users = decodedResponse
+                print("decoded end")
+            } else {
+                print("fail to decode")
+            }
+        } catch {
+            print("Invalid data")
+        }
+        
     }
 }
 
