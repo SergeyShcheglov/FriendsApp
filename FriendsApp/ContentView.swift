8dd7bc5d3e4b27c04757fb7265101e3e7a53ebc2
Sergey Shcheglov <97shcheglov@gmail.com>
2022-02-08 12:48:24 +0600

    first commit

---
+++ FriendsApp/ContentView.swift
@@ -0,0 +1,22 @@
+//
+//  ContentView.swift
+//  FriendsApp
+//
+//  Created by Sergey Shcheglov on 08.02.2022.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        Text("Hello, world!")
+            .padding()
+            .background(.red)
+    }
+}
+
+struct ContentView_Previews: PreviewProvider {
+    static var previews: some View {
+        ContentView()
+    }
+}
---
+++ FriendsApp/FriendsAppApp.swift
@@ -0,0 +1,17 @@
+//
+//  FriendsAppApp.swift
+//  FriendsApp
+//
+//  Created by Sergey Shcheglov on 08.02.2022.
+//
+
+import SwiftUI
+
+@main
+struct FriendsAppApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
